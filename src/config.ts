4ffbdcfc8601bca65b643e59cc8b6704f73954ac
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-07-17 20:58:50 +0800

    feat: init

---
+++ src/config.ts
@@ -0,0 +1,16 @@
+export const KNOWN_LANGUAGES = {
+  English: "en",
+};
+
+export const SIDEBAR = {
+  en: [
+    { text: "", header: true },
+    { text: "Section Header", header: true },
+    { text: "Introduction", link: "en/introduction" },
+    { text: "Page 2", link: "en/page-2" },
+    { text: "Page 3", link: "en/page-3" },
+
+    { text: "Another Section", header: true },
+    { text: "Page 4", link: "en/page-4" },
+  ],
+};

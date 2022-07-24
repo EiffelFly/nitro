1a5b1d4628034177724747d4dad5ab6f396a8cbc
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-07-24 16:48:56 +0800

    feat: add vue PageContent

---
+++ examples/tailwind-simple/src/components/VitePress/PageContent.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+const props = defineProps<{
+  title: string;
+}>();
+</script>
+
+<template>
+  <article id="article" class="p-0 w-full h-full flex flex-col">
+    <section class="prose mb-16">
+      <h1 class="content-title" id="overview">{{ props.title }}</h1>
+      <slot></slot>
+    </section>
+  </article>
+</template>

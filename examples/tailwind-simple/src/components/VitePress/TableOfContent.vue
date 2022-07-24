b522dbb98a23971e054eb2abe81a47abdc712c2a
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-07-24 16:49:21 +0800

    feat: add Vue TableOfContent

---
+++ examples/tailwind-simple/src/components/VitePress/TableOfContent.vue
@@ -0,0 +1,31 @@
+<script lang="ts" setup>
+import { AstroPropHeader } from "../../types/astro";
+import { SIDEBAR } from "../../nitro.config";
+
+const props = defineProps<{
+  headers: AstroPropHeader[];
+}>();
+
+const displayHeaders = props.headers.filter(
+  ({ depth }) =>
+    depth >= SIDEBAR.rightSidebar.headerMinDepth &&
+    depth <= SIDEBAR.rightSidebar.headerMaxDepth
+);
+</script>
+
+<template>
+  <h2 class="mb-4">On this page</h2>
+  <ul>
+    <li
+      v-for="header in displayHeaders"
+      class="group text-sm py-0.5 my-2.5 pl-4 border-l-2 border-slate-300 hover:border-slate-700"
+    >
+      <a
+        class="truncate block text-slate-600 group-hover:text-slate-900"
+        href="{`#${header.slug}`}"
+      >
+        {{ header.text }}
+      </a>
+    </li>
+  </ul>
+</template>

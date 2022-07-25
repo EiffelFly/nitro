7f0217429c8b9a0cc104a73ab11a7a1d37d6f90f
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-07-25 10:18:58 +0800

    feat: add React Navbar

---
+++ examples/tailwind-simple/src/components/React/Navbar/Item.tsx
@@ -0,0 +1,46 @@
+import { FC } from "react";
+import {
+  isExteriorLink,
+  isInteriorLink,
+  NavbarItem,
+} from "../../../nitro.config";
+
+export type ItemProps = {
+  item: NavbarItem;
+};
+
+const Item: FC<ItemProps> = ({ item }) => {
+  if (isInteriorLink(item)) {
+    return (
+      <a className="text-lg font-medium" href={item.to}>
+        {item.label}
+      </a>
+    );
+  }
+
+  if (isExteriorLink(item)) {
+    return (
+      <a
+        href={item.href}
+        target="_blank"
+        rel="noopener noreferer"
+        className="flex my-auto"
+      >
+        {item.icon ? (
+          <img
+            src={item.icon.src}
+            width={item.icon.width}
+            height={item.icon.height}
+            alt={item.icon.alt}
+          />
+        ) : (
+          <p className="my-auto">{item.label}</p>
+        )}
+      </a>
+    );
+  }
+
+  return null;
+};
+
+export default Item;
---
+++ examples/tailwind-simple/src/components/React/Navbar/Navbar.tsx
@@ -0,0 +1,64 @@
+import { FC, useMemo } from "react";
+import { NavBar, NavbarItem } from "../../../nitro.config";
+import { baseStyles } from "../../../styles/base";
+import Item from "./Item";
+import cn from "clsx";
+
+export type NavbarProps = {
+  navbar: NavBar;
+};
+
+const Navbar: FC<NavbarProps> = ({ navbar }) => {
+  const [leftItems, rightItems] = useMemo(() => {
+    let left: NavbarItem[] = [];
+    let right: NavbarItem[] = [];
+
+    navbar.items.forEach((item) => {
+      if (item.position === "right") {
+        right.push(item);
+      } else {
+        left.push(item);
+      }
+    });
+
+    return [left, right];
+  }, [navbar.items]);
+
+  return (
+    <nav
+      className={cn(
+        "w-full z-10 sticky top-0 flex py-4 px-10 bg-white",
+        baseStyles.headerHeight
+      )}
+      title="Top Navigation"
+    >
+      <div className="flex items-center justify-end gap-x-4 w-full py-2">
+        <div className="logo flex mr-4">
+          <a className="flex flex-row gap-x-3" href="/">
+            <img
+              src={navbar.logo.src}
+              alt={navbar.logo.alt}
+              width={navbar.logo.width}
+              height={navbar.logo.height}
+            />
+            <h1 className="text-xl font-bold">{navbar.title}</h1>
+          </a>
+        </div>
+        <div className="flex-1 grid grid-cols-2">
+          <div className="w-full flex flex-row">
+            {leftItems.map((item) => (
+              <Item item={item} />
+            ))}
+          </div>
+          <div className="w-full flex flex-row justify-end">
+            {rightItems.map((item) => (
+              <Item item={item} />
+            ))}
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;

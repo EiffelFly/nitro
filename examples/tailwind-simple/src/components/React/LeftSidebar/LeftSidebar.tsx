c354588388852faa1b29b357f404c40b1bfdec22
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-07-25 12:08:24 +0800

    feat: add React components (#15)
    * feat: add React LeftSidebar
    
    * feat: add React Navbar
    
    * feat: add PageContent
    
    * fix: remove baseStyle destruction and fix some props
    
    * feat: deprecate React PageContent and use Astro, because <slot /> issue
    
    * feat: add RightSidebar and TableOfContent
    
    * chore: remove FC type
    
    * feat: add ReactMainLayout

---
+++ examples/tailwind-simple/src/components/React/LeftSidebar/LeftSidebar.tsx
@@ -0,0 +1,58 @@
+import { useMemo } from "react";
+import { Sidebar, SidebarSection } from "../../../nitro.config";
+import cn from "clsx";
+import Section from "./Section";
+import { baseStyles } from "../../../styles/base";
+
+export type LeftSidebarProps = {
+  leftSidebar: Sidebar["leftSidebar"];
+  currentPagePath: string;
+};
+
+const LeftSidebar = ({ leftSidebar, currentPagePath }: LeftSidebarProps) => {
+  const sidebarSections = useMemo(
+    () =>
+      leftSidebar.items.reduce<SidebarSection[]>((col, item, i) => {
+        // If the first item is not a section header, create a new container section.
+        if (i === 0) {
+          if (!item.header) {
+            const pesudoSection = { text: "" };
+            col.push({ ...pesudoSection, children: [], collapsible: true });
+          }
+        }
+        if (item.header) {
+          col.push({
+            ...item,
+            children: [],
+            collapsible: item.collapsible ?? false,
+          });
+        } else {
+          col[col.length - 1].children.push(item);
+        }
+        return col;
+      }, []),
+    [leftSidebar.items]
+  );
+
+  return (
+    <nav
+      className={cn(
+        "sticky w-full flex flex-col",
+        baseStyles.sidebar.topOffset
+      )}
+    >
+      {sidebarSections.map((section) => (
+        <div className="w-full border-t">
+          <Section
+            items={section.children}
+            text={section.text}
+            collapsible={section.collapsible}
+            currentPagePath={currentPagePath}
+          />
+        </div>
+      ))}
+    </nav>
+  );
+};
+
+export default LeftSidebar;
---
+++ examples/tailwind-simple/src/components/React/Navbar/Item.tsx
@@ -0,0 +1,45 @@
+import {
+  isExteriorLink,
+  isInteriorLink,
+  NavbarItem,
+} from "../../../nitro.config";
+
+export type ItemProps = {
+  item: NavbarItem;
+};
+
+const Item = ({ item }: ItemProps) => {
+  if (isInteriorLink(item)) {
+    return (
+      <a className="text-lg font-medium" href={item.to}>
+        {item.label}
+      </a>
+    );
+  }
+
+  if (isExteriorLink(item)) {
+    return (
+      <a
+        href={item.href}
+        target="_blank"
+        rel="noopener noreferer"
+        className="flex my-auto"
+      >
+        {item.icon ? (
+          <img
+            src={item.icon.src}
+            width={item.icon.width}
+            height={item.icon.height}
+            alt={item.icon.alt}
+          />
+        ) : (
+          <p className="my-auto">{item.label}</p>
+        )}
+      </a>
+    );
+  }
+
+  return null;
+};
+
+export default Item;

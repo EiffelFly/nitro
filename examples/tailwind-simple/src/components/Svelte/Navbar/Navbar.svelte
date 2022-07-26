d775e35eda970a66f0335858693dafa30423eada
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-07-26 09:46:43 +0800

    chore: remove template tag at svelte

--- examples/tailwind-simple/src/components/Svelte/Navbar/Navbar.svelte
+++ examples/tailwind-simple/src/components/Svelte/Navbar/Navbar.svelte
@@ -19,38 +19,33 @@
   });
 </script>
 
-<template>
-  <nav
-    class={cn(
-      "w-full z-10 sticky top-0 flex py-4 px-10 bg-white",
-      headerHeight
-    )}
-    title="Top Navigation"
-  >
-    <div class="flex items-center justify-end gap-x-4 w-full py-2">
-      <div class="logo flex mr-4">
-        <a class="flex flex-row gap-x-3" href="/">
-          <img
-            src={NAVBAR.logo.src}
-            alt={logo.alt}
-            width={logo.width}
-            height={logo.height}
-          />
-          <h1 class="text-xl font-bold">{title}</h1>
-        </a>
+<nav
+  class={cn("w-full z-10 sticky top-0 flex py-4 px-10 bg-white", headerHeight)}
+  title="Top Navigation"
+>
+  <div class="flex items-center justify-end gap-x-4 w-full py-2">
+    <div class="logo flex mr-4">
+      <a class="flex flex-row gap-x-3" href="/">
+        <img
+          src={NAVBAR.logo.src}
+          alt={logo.alt}
+          width={logo.width}
+          height={logo.height}
+        />
+        <h1 class="text-xl font-bold">{title}</h1>
+      </a>
+    </div>
+    <div class="flex-1 grid grid-cols-2">
+      <div class="w-full flex flex-row">
+        {#each leftItems as item}
+          <Item {item} />
+        {/each}
       </div>
-      <div class="flex-1 grid grid-cols-2">
-        <div class="w-full flex flex-row">
-          {#each leftItems as item}
-            <Item {item} />
-          {/each}
-        </div>
-        <div class="w-full flex flex-row justify-end">
-          {#each rightItems as item}
-            <Item {item} />
-          {/each}
-        </div>
+      <div class="w-full flex flex-row justify-end">
+        {#each rightItems as item}
+          <Item {item} />
+        {/each}
       </div>
     </div>
-  </nav>
-</template>
+  </div>
+</nav>
---
+++ examples/tailwind-simple/src/components/Svelte/RightSidebar.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+  import cn from "clsx";
+  import { baseStyles } from "../../styles/base";
+  import { AstroPropHeader } from "../../types/astro";
+  import TableOfContent from "./TableOfContent.vue";
+
+  const props = defineProps<{
+    headers: AstroPropHeader[];
+  }>();
+</script>
+
+<template>
+  <nav
+    :class="cn('w-full sticky', baseStyles.sidebar.topOffset)"
+    aria-labelledby="grid-right"
+  >
+    <div class="h-full overflow-auto">
+      <TableOfContent :headers="props.headers" />
+    </div>
+  </nav>
+</template>

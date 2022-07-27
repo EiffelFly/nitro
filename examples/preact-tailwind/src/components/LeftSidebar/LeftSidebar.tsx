9ccdb3e9470f0366fee16d2ac1fcaaa4d8171147
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-07-27 23:29:54 +0800

    feat: add preact folder

---
+++ examples/preact-tailwind/src/components/LeftSidebar/LeftSidebar.tsx
@@ -0,0 +1,62 @@
+import { useMemo } from "preact/hooks";
+import {
+  Sidebar,
+  SidebarSection,
+} from "../../../../tailwind-simple/src/nitro.config";
+import cn from "clsx";
+import Section from "./Section";
+import { baseStyles } from "../../../../tailwind-simple/src/styles/base";
+
+export type LeftSidebarProps = {
+  leftSidebar: Sidebar["leftSidebar"];
+  currentPagePath: string;
+};
+
+const LeftSidebar = ({ leftSidebar, currentPagePath }: LeftSidebarProps) => {
+  const sidebarSections = useMemo(
+    () =>
+      leftSidebar.items.reduce<SidebarSection[]>((col, item, i) => {
+        // If the first item is not a section header, create a new container section.
+        if (i === 0) {
+          if (!item.header) {
+            const pesudoSection = { text: "" };
+            col.push({ ...pesudoSection, children: [], collapsible: true });
+          }
+        }
+        if (item.header) {
+          col.push({
+            ...item,
+            children: [],
+            collapsible: item.collapsible ?? false,
+          });
+        } else {
+          col[col.length - 1].children.push(item);
+        }
+        return col;
+      }, []),
+    [leftSidebar.items]
+  );
+
+  return (
+    <nav
+      className={cn(
+        "sticky w-full flex flex-col",
+        baseStyles.sidebar.topOffset
+      )}
+      aria-labelledby="grid-right"
+    >
+      {sidebarSections.map((section) => (
+        <div key={section.text} className="w-full border-t">
+          <Section
+            items={section.children}
+            text={section.text}
+            collapsible={section.collapsible}
+            currentPagePath={currentPagePath}
+          />
+        </div>
+      ))}
+    </nav>
+  );
+};
+
+export default LeftSidebar;
---
+++ examples/preact-tailwind/src/types/frontmatter.ts
@@ -0,0 +1,19 @@
+/**
+ *
+ * Every time you add another frontmatter data key into markdown, we encourage you put the type here.
+ * You could get some helps from IDE or get more error info from build failed message.
+ */
+
+export type Frontmatter = {
+  title: string;
+  description: string;
+  lang?: string;
+  date?: string;
+  draft?: boolean;
+  author?: string;
+  dir?: string;
+  feature?: {
+    src: string;
+    alt: string;
+  };
+};

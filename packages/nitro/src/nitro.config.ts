b057f6a3075074fe0f1a93ab4cd33e71a9371d48
EiffelFly <57251712+EiffelFly@users.noreply.github.com>
2022-07-28 10:12:14 +0800

    feat: add Site type

--- packages/nitro/src/nitro.config.ts
+++ packages/nitro/src/nitro.config.ts
@@ -1,19 +1,18 @@
-import { NavBar, Sidebar } from "./types";
+import { NavBar, Sidebar, Site } from "./types";
 
-export const SITE = {
+export const site: Site = {
   title: "Documentation",
   description: "Your website description.",
   defaultLanguage: "en_US",
-};
-
-export const OPEN_GRAPH = {
-  image: {
-    src: "https://github.com/withastro/astro/blob/main/assets/social/banner.jpg?raw=true",
-    alt:
-      "astro logo on a starry expanse of space," +
-      " with a purple saturn-like planet floating in the right foreground",
+  openGraph: {
+    image: {
+      src: "https://github.com/withastro/astro/blob/main/assets/social/banner.jpg?raw=true",
+      alt:
+        "astro logo on a starry expanse of space," +
+        " with a purple saturn-like planet floating in the right foreground",
+    },
+    twitter: "astrodotbuild",
   },
-  twitter: "astrodotbuild",
 };
 
 export const SIDEBAR: Sidebar = {
--- packages/nitro/src/types.ts
+++ packages/nitro/src/types.ts
@@ -70,3 +70,16 @@ export const isExteriorLink = (
   if ("href" in item) return true;
   return false;
 };
+
+export type Site = {
+  title: string;
+  description: string;
+  defaultLanguage: string;
+  openGraph: {
+    image: {
+      src: string;
+      alt: string;
+    };
+    twitter: string;
+  };
+};
